dd4f837902b29a36e423bce904fb6769d3ac1e82
Mupu <2691299+Mupu@users.noreply.github.com>
2019-10-17 18:08:11 +0200

    added support for clang and msvc (not tested)

--- .vscode/build.py
+++ .vscode/build.py
@@ -2,63 +2,123 @@ import sys
 import json
 import os
 
-with open(".vscode/c_cpp_properties.json", "r") as tasks_json:
-    tasks = json.load(tasks_json)
-    configs = tasks["configurations"]
-    
-    # load chosen config
-    config = None
-    for i in range(len(configs)):
-        if configs[i]["name"] == sys.argv[1]:
-            config = tasks["configurations"][i]
-            break
+defaultCompilerCmds = {
+    "gcc" : {
+        "includeCmd"    :       "-I ",
+        "defineCmd"     :       "-D ",
+        "langStdCmd"    :       "-std="
+    },
+    "clang" : {
+        # Not tested!
+        "includeCmd"    :       "-I ",
+        "defineCmd"     :       "-D ",
+        "langStdCmd"    :       "-std="
+    },
+    "msvc" : {
+        # Not tested!
+        "includeCmd"    :       "/I",
+        "defineCmd"     :       "/D",
+        "langStdCmd"    :       "/std:"
+    }
+}
 
-    if config is None:
-        print("Config not found! Please select a existing config. (2. argument in your task that starts build.py)")
+args = {}
+for i in range(len(sys.argv)):
+    # skip name of python script
+    if i > 0:
+        try:
+            split = sys.argv[i].split("=", 1)
+            args[split[0]] = split[1]
+        except IndexError as err:
+            print("Error: Illegal argument '" + sys.argv[i] + "' Please check the docs.")
+            exit(1)
+        
+print("Args: " + str(args))
+
+if args.get("configName") is None:
+        print("Error: Argument 'configName' missing! Please add it to the tasks arguments.")
         exit(1)
 
-    # load config data
-    defines = config.get("defines")
-    includes = config.get("includePath")
-    cstandard = config.get("cStandard")
-    cppstandard = config.get("cppStandard")
-    compilerArgs = config.get("compilerArgs")
-    compilerPath = config.get("compilerPath")
+try:
+    with open(".vscode/c_cpp_properties.json", "r") as tasks_json:
+        tasks = json.load(tasks_json)
+        configs = tasks["configurations"]
+        
+        # load chosen config
+        config = None
+        for i in range(len(configs)):
+            if configs[i]["name"] == args["configName"]:
+                config = tasks["configurations"][i]
+                break
 
-    if compilerPath is None:
-        print("Compiler path not found! Please select a existing compiler in the C/C++ options.")
-        exit(1)
+        if config is None:
+            print("Error: Config not found! Please select a existing config. (Task argument: configName=YOUR_CONFIG_NAME)")
+            exit(1)
 
-    # add compiler to be used
-    cmd = compilerPath
+        # load config data
+        defines = config.get("defines")
+        includes = config.get("includePath")
+        cstandard = config.get("cStandard")
+        cppstandard = config.get("cppStandard")
+        compilerArgs = config.get("compilerArgs")
+        compilerPath = config.get("compilerPath")
+        intelliSenseMode = config.get("intelliSenseMode") # used for automatic lookup table detection
+        compiler = None
 
-    # add compilerArgs
-    if compilerArgs is not None:
-        for a in compilerArgs:
-            cmd += " " + a
+        # choose compiler
+        if args.get("compiler") is None:
+            if intelliSenseMode.startswith("gcc"):
+                compiler = "gcc"
+            elif intelliSenseMode.startswith("clang"):
+                compiler = "clang"
+            else: compiler = "msvc"
+        else:
+            compiler = args["compiler"]
+        
+        
+        # overwrite defaults of chosen compiler
+        print("Used defaultCompilerCmds: " + compiler)
+        for k,v in args.items():
+            defaultCompilerCmds[compiler][k] = v
 
-    # add defines
-    if compilerArgs is not None:
-        for d in defines:
-            cmd += " -D " + d
- 
-    # add include paths
-    if compilerArgs is not None:
-        for i in includes:
-            cmd += " -I " + i
+        if compilerPath is None:
+            print("Error: Compiler path not found! Please select a existing compiler in the C/C++ options.")
+            exit(1)
+
+        # add compiler to be used
+        cmd = compilerPath
+
+        # add compilerArgs
+        if compilerArgs is not None:
+            for a in compilerArgs:
+                cmd += " " + a
+
+        # add defines
+        if compilerArgs is not None:
+            for d in defines:
+                cmd += " " + defaultCompilerCmds[compiler]["defineCmd"] + d
     
-    # add cStandard/c++Standard
-    if cstandard and compilerPath.endswith("gcc.exe"):
-        cmd += " -std=" + cstandard
-    elif cppstandard and compilerPath.endswith("g++.exe"):
-        cmd += " -std=" + cppstandard
+        # add include paths
+        if compilerArgs is not None:
+            for i in includes:
+                cmd += " " + defaultCompilerCmds[compiler]["includeCmd"] + i
+        
+        # add cStandard/c++Standard
+        if args["lang"].upper() == "C":
+            cmd += " " + defaultCompilerCmds[compiler]["langStdCmd"] + cstandard
+        elif args["lang"].upper() == "CPP":
+            cmd += " " + defaultCompilerCmds[compiler]["langStdCmd"] + cppstandard
+        
 
-    # run command
-    print("Running command: " + cmd)
+        # run command
+        print("Running command: " + cmd)
 
-    # create out folder
-    if not os.path.exists("out"): os.makedirs("out")
+        # create out folder
+        if not os.path.exists("out"): os.makedirs("out")
 
-    statusCode = os.system(cmd)
-    print("The build process has exited with code " + str(statusCode) + ".")
-    exit(statusCode)
+        statusCode = os.system(cmd)
+        print("The build process has exited with code " + str(statusCode) + ".")
+        exit(statusCode)
+except FileNotFoundError as err:
+    print("Error: File c_cpp_properties.json not found. Please install the Microsoft C/C++ extension and try again.")
+    exit(1)
--- README.md
+++ README.md
@@ -1,7 +1,45 @@
 # VSCode C/C++ GCC AutoSynch
 
+## Description
+This script can be used to pass the general settings of c_cpp_properties.json to a task aka your compiler (e.g. GCC). If you want to contribute to extend the functionality you may do so. Just send a PR.
+
 ## Usage
 ```
-# test
-test
+# Clone this project.
+
+# Edit your C/C++ Properties to fit your needs (Advanced settings are not supported yet)
+
+# If you change your configuration name or add new ones in c_cpp_properties.json you can select them by passing the name of the configuration to the script. You do this by adding "configName=NAME_HERE" to the tasks arguments.(shown in [Pic 1]) It is important that you add the arguments AFTER .vscode/build.py!
+
+# Edit miDebuggerPath in launcher.json. Here you add the path to your debugger e.g. "C:\\mingw-w64\\mingw32\\bin\\gdb.exe".
+
+# To change your config from Win64_Debug to Win64_Release for example you'd change [Pic 2] to 'Win64_Release' and then the build [Pic 3] to 'Build'.
+The first change changes the actual config for the compile (the resulting .exe is effected). The second change is for the Intellisense in the IDE.
+
+# Compile :)
 ```
+
+## Supported arguments
+```
+Shown in [Pic 1].
+
+configName=YOUR_CONFIG_NAME - REQUIRED - selects the config that will be loaded from c_cpp_properties.json.
+
+compiler=[clang/gcc/msvc] - OPTIONAL - changes the default commandline lookup table. Can also be used if the detection is wrong.
+
+lang=[C/CPP] - OPTIONAL - if used it will add the chosen langunge level of c/cpp of c_cpp_properties.json to the arguments .
+
+includeCmd=YOUR_OPTION - OPTIONAL - overwrites the default tag for includes (i.e. "-I ")
+defineCmd=YOUR_OPTION - OPTIONAL - overwrites the default tag for includes (i.e. "-D ")
+langStdCmd=YOUR_OPTION - OPTIONAL - overwrites the default tag for the language standard version (i.e. "-std= ")
+
+```
+
+[Pic 1]
+![alt text](https://i.imgur.com/gnoDmBw.png)
+
+[Pic 2]
+![alt text](https://i.imgur.com/tjWqrUe.png)
+
+[Pic 3]
+![alt text](https://i.imgur.com/jZbP2uh.png)
